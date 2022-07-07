eb44a192ce760f7000aa035a198d0882feff409f
Edward Felipe III <edomfel3@gmail.com>
2022-07-07 10:25:50 -1000

    Delete event endpoint added

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -12,6 +12,40 @@ const router = express.Router();
 const validateEvent = []
 
 
+//DELETE a specific event
+router.delete(
+    '/:eventId',
+    requireAuth,
+    async (req, res, next) => {
+        const event = await Event.findByPk(req.params.eventId)
+        const group = await event.getGroup()
+        const membership = await Member.findOne({
+            where: {
+                groupId: group.id,
+                memberId: req.user.id
+            },
+        })
+
+        //Event cannot be found
+        if (!event) {
+            const err = new Error('Event couldn\'t be found')
+            err.status = 404
+            return next(err)
+        }
+        //Only the owner and cohosts can delete the event
+        if (group.organizerId === req.user.id || (membership && membership.status === 'co-host')) {
+
+            await event.destroy()
+            res.json({ message: "Successfully deleted" })
+
+        } else {
+            const err = new Error('You must be either the owner or a co-host to delete this event')
+            err.status = 403
+            return next(err)
+        }
+    }
+)
+
 //GET a specific event
 // including numAttending, previewImage, Group, and Venus
 router.get(
@@ -19,7 +53,7 @@ router.get(
     async (req, res, next) => {
         const event = Event.findByPk(req.params.eventId)
 
-        if (!event){
+        if (!event) {
             const err = new Error('Event couldn\'t be found')
             err.status = 404
             return next(err)
@@ -34,9 +68,9 @@ router.get(
 router.get(
     '/',
     async (req, res, next) => {
-        const events = await Event.findAll({ attributes: { exclude: ['eventId']}})
+        const events = await Event.findAll({ attributes: { exclude: ['eventId'] } })
 
-        res.json({Events: events})
+        res.json({ Events: events })
     })
 
 module.exports = router
