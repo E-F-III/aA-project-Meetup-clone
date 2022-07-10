36cee55825053341a381f3cae031ded09a7394ee
Edward Felipe III <edomfel3@gmail.com>
2022-07-10 13:49:40 -1000

    Delete an image

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const groupsRouter = require('./groups.js')
 const eventsRouter = require('./events.js')
 const venuesRouter = require('./venues.js')
+const imagesRouter = require('./images.js')
 const { restoreUser } = require("../../utils/auth.js");
 
 router.use(restoreUser);
@@ -17,6 +18,8 @@ router.use('/groups', groupsRouter);
 router.use('/events', eventsRouter)
 
 router.use('/venues', venuesRouter)
+
+router.use('/images', imagesRouter)
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
