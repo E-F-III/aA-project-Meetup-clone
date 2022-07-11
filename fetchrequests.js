a99f7bb5b84081556378643396f9fdb9385f4e73
Edward Felipe III <edomfel3@gmail.com>
2022-07-10 14:59:18 -1000

    push test

--- fetchrequests.js
+++
@@ -1,9 +0,0 @@
-//log in
-fetch('/api/session', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `khMXYfMG-vNSPikyKla2Vt4eckfGtNopPQ8w`
-    },
-    body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
-  }).then(res => res.json()).then(data => console.log(data));
