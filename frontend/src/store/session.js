15a1300b42b4a0d397844280e9037d46716187ec
Edward Felipe III <edomfel3@gmail.com>
2022-07-22 07:51:00 -1000

    Signup action and SignupFormPage component

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -37,6 +37,22 @@ export const restoreUser = () => async dispatch => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
