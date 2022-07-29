cb312b975cd3cb048c1b208e1278603f29d69df2
Edward Felipe III <edomfel3@gmail.com>
2022-07-28 20:22:54 -1000

    Adjusted start date displays on events list

--- frontend/src/components/EventsList/index.js
+++ frontend/src/components/EventsList/index.js
@@ -26,7 +26,17 @@ function EventsList() {
                     {eventsList.map(event => {
 
                         const eventStart = new Date (event.startDate)
-                        const startString = eventStart.toString()
+                        const startArray = eventStart.toString().split(' ')
+
+                        // const timezone = eventStart.toTimeString().split(' ')[3]
+
+                        const day = startArray[0]
+                        const date = `${startArray[1]} ${startArray[2]}`
+                        const hours24 = eventStart.getHours()
+                        const hours = Number(hours24) > 12 ? Number(hours24) - 12 : hours24
+                        const minutesDefault = eventStart.getMinutes()
+                        const minutes = minutesDefault.length === 2 ? minutesDefault : '0'.concat(minutesDefault)
+                        const AMPM =  Number(hours24) > 12 ? 'PM' : 'AM'
 
                         return (
                             <NavLink className='navLink' to={`/events/${event.id}`} key={event.id}>
@@ -36,7 +46,7 @@ function EventsList() {
                                     </div>
                                     <div>
                                         <div className='card-title'>
-                                            <h3>{startString}</h3>
+                                            <h3>{`${day}, ${date} • ${hours}:${minutes} ${AMPM}`}</h3>
                                             <h3>{event.name}</h3>
                                             <h4>{event.Group.name} • {event.Group.city}, {event.Group.state}</h4>
                                         </div>
--- frontend/src/components/GroupsAndEventsNavBar/index.js
+++ frontend/src/components/GroupsAndEventsNavBar/index.js
@@ -11,10 +11,10 @@ function GroupsEventsNav() {
     return (
         <div className='GroupsEventsNav'>
             <div className='listNav'>
-                <NavLink to='/groups' className='navLink header' activeClassName='tab-active'>Groups</NavLink>
+                <NavLink to='/events' className='navLink header' activeClassName='tab-active'>Events</NavLink>
             </div>
             <div className='listNav'>
-                <NavLink to='/events' className='navLink header' activeClassName='tab-active'>Events</NavLink>
+                <NavLink to='/groups' className='navLink header' activeClassName='tab-active'>Groups</NavLink>
             </div>
         </div>
     )
