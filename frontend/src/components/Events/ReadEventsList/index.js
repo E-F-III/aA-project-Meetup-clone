276fb1cfd4ffd30dfdf37cfce0bd5ab07e3bb429
Edward Felipe III <edomfel3@gmail.com>
2022-08-24 06:47:33 -1000

    Created events list component

--- frontend/src/components/Events/ReadEventsList/index.js
+++ frontend/src/components/Events/ReadEventsList/index.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import './Events.css'
-import { NavLink } from 'react-router-dom';
+import { getEventsThunk } from '../../../store/Events';
 
-import FooterInfo from '../../FooterInfo'
+
+import './Events.css'
 
 function EventsList() {
     const dispatch = useDispatch()
     const events = useSelector(state => state.events)
-    // let eventsList = Object.values(events)
+    const eventsList = Object.values(events)
 
     const [isLoaded, setIsLoaded] = useState(false)
 
+    useEffect(() => {
+        dispatch(getEventsThunk())
+        .then(() => setIsLoaded(true))
+    }, [dispatch])
 
     return isLoaded && (
         <>
+        {eventsList.map(event => (
+            <EventCard event={event} />
+        ))}
         </>
     )
 }
