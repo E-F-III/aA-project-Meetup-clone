252083c43a819bf754ff011faddced4d5ebc359c
Edward Felipe III <edomfel3@gmail.com>
2022-08-25 12:56:21 -1000

    Group/Event list NavBar css

--- frontend/src/components/FindEventsAndGroups/ListsNav/index.js
+++ frontend/src/components/FindEventsAndGroups/ListsNav/index.js
@@ -8,10 +8,10 @@ function ListsNav() {
     return (
         <div className='GroupsEventsNav'>
             <div className='listNav'>
-                <NavLink to='/find/events' className='navLink header' activeClassName='tab-active'>Events</NavLink>
+                <NavLink to='/find/events' className='navLink textcolor-bluegreen text25' activeClassName='tab-active'>Events</NavLink>
             </div>
             <div className='listNav'>
-                <NavLink to='/find/groups' className='navLink header' activeClassName='tab-active'>Groups</NavLink>
+                <NavLink to='/find/groups' className='navLink textcolor-bluegreen text25' activeClassName='tab-active'>Groups</NavLink>
             </div>
         </div>
     )
